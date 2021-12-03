d03ffc6e5ebebcf405e7511472d7fbd81b9c5688
SashaVoloshyn <sasha.voloshyn359@gmail.com>
2021-12-03 18:43:32 +0200

    lesson 9

---
+++ lesson9/add.js
@@ -0,0 +1,80 @@
+
+    let usersList = [
+        {
+            id: 1,
+            name: 'Leanne Graham',
+            username: 'Bret',
+            email: 'Sincere@april.biz',
+            address: {
+                street: 'Kulas Light',
+                suite: 'Apt. 556',
+                city: 'Gwenborough',
+                zipcode: '92998-3874',
+                geo: {
+                    lat: '-37.3159',
+                    lng: '81.1496'
+                }
+            },
+            phone: '1-770-736-8031 x56442',
+            website: 'hildegard.org',
+            company: {
+                name: 'Romaguera-Crona',
+                catchPhrase: 'Multi-layered client-server neural-net',
+                bs: 'harness real-time e-markets'
+            }
+        },
+    ]
+    // - Є масив
+    // Створити під кожен елемент окремий блок.
+    // В цьому блоці, під кожну властивість, та властивості внутрішніх об'єктів створити свої окремі блок.
+    for (const userElement of usersList) {
+        let users = document.createElement('div');
+        users.classList.add('user');
+
+        let id = document.createElement('div');
+        id.innerText = userElement.id;
+
+        let name = document.createElement('div');
+        name.innerText = userElement.name;
+
+        let username = document.createElement('div');
+        username.innerText = userElement.username;
+
+        let email = document.createElement('div');
+        email.innerText = userElement.email;
+
+        let phone = document.createElement('div');
+        phone.innerText = userElement.phone;
+
+        let website = document.createElement('div');
+        website.innerText = userElement.website;
+
+        let company = document.createElement('div');
+        for (const companyEl of userElement.company) {
+            let companyDiv = document.createElement('div');
+            let txt = '';
+            for (const inEl in companyEl) {
+                txt += `${inEl}: ${companyEl[inEl]}`;
+            }
+            companyDiv.innerText = txt;
+            company.appendChild(companyDiv);
+
+        }
+
+
+        users.appendChild(id);
+        users.appendChild(name);
+        users.appendChild(username);
+        users.appendChild(email);
+        users.appendChild(phone);
+        users.appendChild(website);
+        users.appendChild(company);
+
+
+
+        document.body.appendChild(users);
+
+    }
+
+
+
